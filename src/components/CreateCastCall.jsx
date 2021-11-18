34bc89472d0c953f2ff7a122f8f3b601c43e19f2
angealchw2571 <angealty96@gmail.com>
2021-11-18 10:35:19 +0800

    finished form

--- src/components/CreateCastCall.jsx
+++ src/components/CreateCastCall.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 function CreateCastCall() {
-
   const handleSubmit = (event) => {
     event.preventDefault();
     const formData = {
@@ -17,158 +16,185 @@ function CreateCastCall() {
       company: event.target.company.value,
       remnueration: event.target.remnueration.value,
       loading_scale: event.target.loading_scale.value,
-      contract: (event.target.contract.value) === "true" ? (true) : (false),
-      
-      
+      contract: event.target.contract.value === "true" ? true : false,
     };
     console.log("result", formData);
   };
 
   return (
-    <div className="grid grid-cols-6 text-white justify-items-center">
-      <div className="secondDiv col-span-4">
-        <form className="flex flex-wrap max-w-md text-black" onSubmit={handleSubmit}>
-          <div className="relative w-full mb-3">
-            <label className="block uppercase text-white text-xs font-bold mb-2">
-              Casting Type
-            </label>
-            <select name= "type"  className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full">
-            <option value="commercial">Commercial</option>
-            <option value="government">Government</option>
-            <option value="theatre">Theatre</option>
-            <option value="modelling">Modelling</option>
-          </select>
+    <div className="grid grid-cols-1  text-white justify-items-center">
+      <form
+        className="grid-cols-12 col-span-1 flex gap-4 text-white max-w-lg"
+        onSubmit={handleSubmit}
+      >
+        <div className="firstDiv col-span-4 ">
+          <div className="flex flex-wrap max-w-md">
+            <div className="relative w-full mb-3">
+              <label className="block uppercase text-white text-xs font-bold mb-2">
+                Casting Type
+              </label>
+              <select
+                name="type"
+                className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
+              >
+                <option value="commercial">Commercial</option>
+                <option value="government">Government</option>
+                <option value="theatre">Theatre</option>
+                <option value="modelling">Modelling</option>
+              </select>
+            </div>
+            <div className="relative w-full mb-3">
+              <label className="block uppercase text-white text-xs font-bold mb-2">
+                Platform
+              </label>
+              <select
+                name="platform"
+                className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
+              >
+                <option value="tv">TV</option>
+                <option value="radio">Radio</option>
+                <option value="print">Print/Magazine/Poster</option>
+              </select>
+            </div>
+            <div className="relative w-full mb-3">
+              <label className="block uppercase text-white text-xs font-bold mb-2">
+                Period in months
+              </label>
+              <input
+                name="period"
+                type="number"
+                min="1"
+                className="border-0 px-3 py-3 placeholder-white text-gray-700 bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
+                placeholder="Period in months"
+              />
+            </div>
+            <div className="relative w-full mb-3">
+              <label className="block uppercase text-white text-xs font-bold mb-2">
+                Role
+              </label>
+              <input
+                name="role"
+                className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
+                placeholder="Role"
+              />
+            </div>
+            <div className="relative w-full mb-3">
+              <label className="block uppercase text-white text-xs font-bold mb-2">
+                Gender
+              </label>
+              <select
+                name="gender"
+                className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
+              >
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
+            </div>
+            <div className="relative w-full mb-3">
+              <label className="block uppercase text-white text-xs font-bold mb-2">
+                Contact
+              </label>
+              <input
+                name="contact"
+                className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
+                placeholder="Contact"
+              />
+            </div>
+            <div className="relative w-full mb-3">
+              <label className="block uppercase text-white text-xs font-bold mb-2">
+                Location
+              </label>
+              <input
+                name="location"
+                className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
+                placeholder="Location"
+              />
+            </div>
+
+            <div className="text-center mt-6">
+              <button
+                className="bg-gold-light text-gray-800 active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
+                type="submit"
+                style={{ transition: "all .15s ease" }}
+              >
+                Post gig
+              </button>
+            </div>
           </div>
-          <div className="relative w-full mb-3">
-            <label className="block uppercase text-white text-xs font-bold mb-2">
-              Platform
-            </label>
-            <select name= "platform"  className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full">
-            <option value="tv">TV</option>
-            <option value="radio">Radio</option>
-            <option value="print">Print/Magazine/Poster</option>
-          </select>
+        </div>
+        <div className="secondDiv col-span-4">
+          <div className="flex flex-wrap max-w-md">
+            <div className="relative w-full mb-3">
+              <label className="block uppercase text-white text-xs font-bold mb-2">
+                Posted by
+              </label>
+              <input
+                name="posted_by"
+                className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
+                placeholder="Posted By"
+              />
+            </div>
+            <div className="relative w-full mb-3">
+              <label className="block uppercase text-white text-xs font-bold mb-2">
+                Company
+              </label>
+              <input
+                name="company"
+                className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
+                placeholder="Company"
+              />
+            </div>
+            <div className="relative w-full mb-3">
+              <label className="block uppercase text-white text-xs font-bold mb-2">
+                Remnueration (SGD)
+              </label>
+              <input
+                name="remnueration"
+                className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
+                placeholder="Remnueration"
+              />
+            </div>
+            <div className="relative w-full mb-3">
+              <label className="block uppercase text-white text-xs font-bold mb-2">
+                Loading scale
+              </label>
+              <select
+                name="loading_scale"
+                className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
+              >
+                <option value="local">Local</option>
+                <option value="global">Global</option>
+              </select>
+            </div>
+            <div className="relative w-full mb-3">
+              <label className="block uppercase text-white text-xs font-bold mb-2">
+                Contract
+              </label>
+              <select
+                name="contract"
+                className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
+              >
+                <option value="true">Yes</option>
+                <option value="false">No</option>
+              </select>
+            </div>
           </div>
-          <div className="relative w-full mb-3">
-            <label className="block uppercase text-white text-xs font-bold mb-2">
-              Period in months
-            </label>
-            <input
-              name="period"
-              type="number"
-              min="1"
-              className="border-0 px-3 py-3 placeholder-white text-gray-700 bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
-              placeholder="Period in months"
-            />
-          </div>
-          <div className="relative w-full mb-3">
-            <label className="block uppercase text-white text-xs font-bold mb-2">
-              Role
-            </label>
-            <input
-              name="role"
-              className="border-0 px-3 py-3 placeholder-white text-gray-700 bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
-              placeholder="Role"
-            />
-          </div>
-          <div className="relative w-full mb-3">
-            <label className="block uppercase text-white text-xs font-bold mb-2">
-              Gender
-            </label>
-            <select name= "gender"  className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full">
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-          </select>
-          </div>
-          <div className="relative w-full mb-3">
-            <label className="block uppercase text-white text-xs font-bold mb-2">
-              Contact
-            </label>
-            <input
-              name="contact"
-              className="border-0 px-3 py-3 placeholder-white text-gray-700 bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
-              placeholder="Contact"
-            />
-          </div>
-          <div className="relative w-full mb-3">
-            <label className="block uppercase text-white text-xs font-bold mb-2">
-              Location
-            </label>
-            <input
-              name="location"
-              className="border-0 px-3 py-3 placeholder-white text-gray-700 bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
-              placeholder="Location"
-            />
-          </div>
-          <div className="relative w-full mb-3">
-            <label className="block uppercase text-white text-xs font-bold mb-2">
-              Posted by
-            </label>
-            <input
-              name="posted_by"
-              className="border-0 px-3 py-3 placeholder-white text-gray-700 bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
-              placeholder="Posted By"
-            />
-          </div>
-          <div className="relative w-full mb-3">
-            <label className="block uppercase text-white text-xs font-bold mb-2">
-              Company
-            </label>
-            <input
-              name="company"
-              className="border-0 px-3 py-3 placeholder-white text-gray-700 bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
-              placeholder="Company"
-            />
-          </div>
-          <div className="relative w-full mb-3">
-            <label className="block uppercase text-white text-xs font-bold mb-2">
-              Remnueration (SGD)
-            </label>
-            <input
-              name="remnueration"
-              className="border-0 px-3 py-3 placeholder-white text-gray-700 bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
-              placeholder="Remnueration"
-            />
-          </div>
-          <div className="relative w-full mb-3">
-            <label className="block uppercase text-white text-xs font-bold mb-2">
-              Loading scale
-            </label>
-            <select name= "loading_scale"  className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full">
-            <option value="local">Local</option>
-            <option value="global">Global</option>
-          </select>
-          </div>
-          <div className="relative w-full mb-3">
-            <label className="block uppercase text-white text-xs font-bold mb-2">
-              Contract
-            </label>
-            <select name= "contract"  className="border-0 px-3 py-3 placeholder-white text-white bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full">
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-          </select>
-          </div>
+        </div>
+        <div className="thirdDiv col-span-4">
+        <div className="flex flex-wrap max-w-md">
           <div className="relative w-full mb-3">
             <label className="block uppercase text-white text-xs font-bold mb-2">
               Details
             </label>
             <input
               name="details"
-              className="border-0 px-3 py-3 placeholder-white text-gray-700 bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
+              className="border-0 px-3 py-3 placeholder-white text-gray-100 bg-gray-400 rounded text-sm shadow focus:outline-none focus:ring w-full"
               placeholder="Details"
             />
           </div>
-          <div className="text-center mt-6">
-            <button
-              className="bg-gold-light text-gray-800 active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
-              type="submit"
-              style={{ transition: "all .15s ease" }}
-            >
-              Post gig
-            </button>
           </div>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
   );
 }
