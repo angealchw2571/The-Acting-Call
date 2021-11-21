f34be103251253bf79a242a504c562ac35aeb40b
angealchw2571 <angealty96@gmail.com>
2021-11-21 12:46:16 +0800

    finished actor carousel

---
+++ acting-call/src/components/ActorCarousel.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
+import SwiperCore, { Pagination, Navigation } from "swiper";
+
+SwiperCore.use([Pagination, Navigation]);
+
+function GigsCarousel(props) {
+  const actorData = props.props;
+  // console.log("actorData", actorData);
+
+  return (
+    <>
+      <Swiper
+        slidesPerView={5}
+        spaceBetween={0}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        className="mySwiper"
+      >
+        {actorData.map((e, i) => {
+          return (
+            <SwiperSlide key={i} className=" group text-center">
+              <div className="imgDiv group-hover:opacity-20">
+                <img className="" src={e.displayPicture} style={{maxHeight:350, maxWidth:250}} />
+              </div> 
+              <div className="nameDiv text-xl opacity-0 absolute bottom-1/2 right-1/2 group-hover:opacity-100">
+               <div className=""> 
+                {e.name}
+               </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
+  );
+}
+
+export default GigsCarousel;
