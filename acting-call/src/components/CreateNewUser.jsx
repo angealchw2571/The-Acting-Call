e3956ee1b9dfaba2e42646035bc3dc3ed7457865
angealchw2571 <angealty96@gmail.com>
2021-11-24 17:30:16 +0800

    working 2nd

--- acting-call/src/components/CreateNewUser.jsx
+++ acting-call/src/components/CreateNewUser.jsx
@@ -13,7 +13,8 @@ function CreateNewUser() {
       'Access-Control-Allow-Credentials': true,
       'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
       'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-    }
+    },
+    baseURL: "https://castingcallbackend.herokuapp.com/"
  }
 
   const notifySuccess = () => toast.success("Success!", {
